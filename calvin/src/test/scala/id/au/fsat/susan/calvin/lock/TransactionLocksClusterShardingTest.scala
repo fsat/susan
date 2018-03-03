6dac0763e1e4e090205b3c2dd391000917ea2b2a
Felix Satyaputra <fsatyaputra@gmail.com>
2018-03-03 22:53:47 +1100

    WIP - scenario where TransactionLock shard crashed, and we expect other shards to continue retain its state

--- calvin/src/test/scala/id/au/fsat/susan/calvin/lock/TransactionLocksClusterShardingTest.scala
+++ calvin/src/test/scala/id/au/fsat/susan/calvin/lock/TransactionLocksClusterShardingTest.scala
@@ -9,6 +9,7 @@ import id.au.fsat.susan.calvin.lock.TransactionLockClusterShardingSettings.{ Rec
 import id.au.fsat.susan.calvin.lock.TransactionLocks._
 import org.scalatest.{ FunSpec, Inside }
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 
 object TransactionLocksClusterShardingTest {
@@ -107,8 +108,67 @@ class TransactionLocksClusterShardingTest extends FunSpec with ClusteredTest wit
     }
   }
 
-  describe("restarts") {
-    it("replays transaction lock operation")(pending)
+  describe("a crash occurred") {
+    it("continues transaction lock operation") {
+      withCluster() { implicit cluster =>
+        val f = testFixture()
+        import f._
+
+        val timeoutObtain = f.txLockSettings.maxTimeoutObtain
+        val timeoutReturn = f.txLockSettings.maxTimeoutReturn
+
+        val client1 = TestProbe()(cluster.nodes.head._1)
+
+        val request1Id = RequestId(UUID.randomUUID())
+        val record1Id = RecordId(1)
+
+        client1.send(txLock1, LockGetRequest(request1Id, record1Id, timeoutObtain, timeoutReturn))
+
+        val lock1 = inside(client1.expectMsgType[LockGetSuccess]) {
+          case LockGetSuccess(lock @ Lock(`request1Id`, `record1Id`, _, createdAt, returnDeadline)) =>
+            createdAt.plusNanos(timeoutReturn.toNanos) shouldBe returnDeadline
+            lock
+        }
+
+        val client2 = TestProbe()(cluster.nodes(1)._1)
+
+        val request2Id = RequestId(UUID.randomUUID())
+        val record2Id = RecordId(2)
+
+        client2.send(txLock2, LockGetRequest(request2Id, record2Id, timeoutObtain, timeoutReturn))
+
+        val lock2 = inside(client2.expectMsgType[LockGetSuccess]) {
+          case LockGetSuccess(lock @ Lock(`request2Id`, `record2Id`, _, createdAt, returnDeadline)) =>
+            createdAt.plusNanos(timeoutReturn.toNanos) shouldBe returnDeadline
+            lock
+        }
+
+        val client3 = TestProbe()(cluster.nodes.last._1)
+
+        val request3Id = RequestId(UUID.randomUUID())
+
+        client3.send(txLock3, LockGetRequest(request3Id, record1Id, timeoutObtain, timeoutReturn))
+        client3.expectNoMessage(100.millis)
+
+        // Crash the first actor system, (hopefully) taking down the shard on the first node
+        Await.result(cluster.nodes.head._1.terminate(), Duration.Inf)
+
+        client2.send(txLock2, LockReturnRequest(lock2))
+        client2.expectMsg(LockReturnSuccess(lock2))
+
+        client3.send(txLock3, LockReturnRequest(lock1))
+        client3.expectMsg(LockReturnSuccess(lock1))
+
+        val lock3 = inside(client3.expectMsgType[LockGetSuccess](10.seconds)) {
+          case LockGetSuccess(lock @ Lock(`request3Id`, `record1Id`, _, createdAt, returnDeadline)) =>
+            createdAt.plusNanos(timeoutReturn.toNanos) shouldBe returnDeadline
+            lock
+        }
+
+        client3.send(txLock3, LockReturnRequest(lock3))
+        client3.expectMsg(LockReturnSuccess(lock3))
+      }
+    }
   }
 
   describe("transaction lock terminated") {
