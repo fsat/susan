c6354b89a487138b9f4c9de9b365fec07e646982
Felix Satyaputra <fsatyaputra@gmail.com>
2018-05-30 17:51:32 +1000

    WIP - CRDT get

--- calvin/src/main/scala/id/au/fsat/susan/calvin/lock/storage/RecordLocksStorage.scala
+++ calvin/src/main/scala/id/au/fsat/susan/calvin/lock/storage/RecordLocksStorage.scala
@@ -4,22 +4,29 @@ import java.util.UUID
 
 import akka.actor.{ Actor, ActorRef, Props, Terminated }
 import akka.cluster.Cluster
-import akka.cluster.ddata.DistributedData
-import id.au.fsat.susan.calvin.StateTransition
+import akka.cluster.ddata.Replicator.ReadAll
+import akka.cluster.ddata._
+import id.au.fsat.susan.calvin.{ RecordId, RemoteMessage, StateTransition }
 import id.au.fsat.susan.calvin.lock.RecordLocks._
 
 import scala.collection.immutable.Seq
+import scala.concurrent.duration._
 
 object RecordLocksStorage {
   val Name = "record-locks-storage"
 
   def props: Props = Props(new RecordLocksStorage)
 
-  //    val Key = ORMapKey[String, RecordLocksStorage]("record-locks")
-  //    val InitialValue = ORMap.empty[String, RecordLocksStorage]
+  case class StoredValue(state: RecordLocksStateToPersist, runningRequest: Option[RunningRequest]) extends ReplicatedData with RemoteMessage {
+    override type T = StoredValue
+    override def merge(that: StoredValue): StoredValue = that
+  }
 
-  //    def update(key: ActorRef, state: RecordLocksState): ORMap[String, RecordLocksState] => ORMap[String, RecordLocksState] = ???
-  //    private def update(entityId: String, state: RecordLocksState): ORMap[String, RecordLocksState] => ORMap[String, RecordLocksState] = ???
+  val Key = ORMapKey[String, StoredValue]("record-locks")
+  val InitialValue = ORMap.empty[String, StoredValue]
+
+  def update(key: ActorRef, state: StoredValue): ORMap[String, StoredValue] => ORMap[String, StoredValue] = ???
+  private def update(entityId: String, state: StoredValue): ORMap[String, StoredValue] => ORMap[String, StoredValue] = ???
 
   sealed trait Message
   sealed trait RequestMessage extends Message
@@ -34,7 +41,16 @@ object RecordLocksStorage {
   case class UpdateStateSuccess(state: RecordLocksStateToPersist, runningRequest: Option[RunningRequest]) extends ResponseMessage
   case class UpdateStateFailure(request: UpdateStateRequest, message: String, error: Option[Throwable]) extends FailureMessage with ResponseMessage
 
-  case class PendingRequest(id: UUID, message: RequestMessage)
+  case class PendingGetRequest(id: UUID, message: GetStateRequest)
+
+  case class ReplicatorMessageWrapper(message: Replicator.ReplicatorMessage) extends RequestMessage
+
+  sealed trait StateData
+  case class IdleStateData(pendingGetRequests: Seq[PendingGetRequest] = Seq.empty)
+  case class UpdateStateData(
+    runningUpdate: UpdateStateRequest,
+    pendingUpdates: Map[RecordId, UpdateStateRequest],
+    pendingGetRequests: Seq[PendingGetRequest] = Seq.empty)
 }
 
 class RecordLocksStorage extends Actor {
@@ -43,17 +59,58 @@ class RecordLocksStorage extends Actor {
   private implicit val node = Cluster(context.system)
   private val replicator = DistributedData(context.system).replicator
 
-  override def receive: Receive = stateTransition(all())
+  override def receive: Receive = stateTransition(idle(IdleStateData()))
 
   private def stateTransition(currentState: StateTransition[RequestMessage]): Receive = {
     case v: RequestMessage =>
       val nextState = currentState.pf(v)
       context.become(stateTransition(if (nextState == StateTransition.stay) currentState else nextState))
 
+    case v: Replicator.ReplicatorMessage =>
+      self ! ReplicatorMessageWrapper(v)
+
     case Terminated(`replicator`) =>
       context.stop(self)
   }
 
-  private def all(): StateTransition[RequestMessage] = all(Seq.empty)
-  private def all(pendingRequest: Seq[PendingRequest]): StateTransition[RequestMessage] = ???
+  protected def recordIdFromRef(ref: ActorRef): String = ???
+
+  private def idle(state: IdleStateData): StateTransition[RequestMessage] =
+    handleGetRequests(Seq.empty)(v => idle(IdleStateData(v)))
+
+  private def handleGetRequests(pendingGetRequests: Seq[PendingGetRequest])(nextState: Seq[PendingGetRequest] => StateTransition[RequestMessage]): StateTransition[RequestMessage] =
+    StateTransition {
+      case v: GetStateRequest =>
+        val pending = PendingGetRequest(UUID.randomUUID(), v)
+        val readConsistency = ReadAll(timeout = 5.seconds)
+        replicator ! Replicator.Get(Key, readConsistency, Some(pending.id.toString))
+        nextState(pendingGetRequests :+ pending)
+
+      case ReplicatorMessageWrapper(v @ Replicator.GetSuccess(`Key`, Some(id: String))) =>
+        val data = v.get(Key)
+        pendingGetRequests
+          .filter(_.id.toString == id)
+          .flatMap { req =>
+            val recordId = recordIdFromRef(req.message.from)
+            data.get(recordId).map(req.message.from -> _)
+          }
+          .foreach { v =>
+            val (from, data) = v
+            from ! GetStateSuccess(data.state, data.runningRequest)
+          }
+
+        nextState(pendingGetRequests.filterNot(_.id.toString == id))
+
+      case ReplicatorMessageWrapper(Replicator.GetFailure(`Key`, Some(id: String))) =>
+        pendingGetRequests
+          .filter(_.id.toString == id)
+          .collect {
+            case PendingGetRequest(_, x: GetStateRequest) => x
+          }
+          .foreach { r =>
+            r.from ! GetStateFailure(r, "Unable to get record locks state", None)
+          }
+
+        nextState(pendingGetRequests.filterNot(_.id.toString == id))
+    }
 }
